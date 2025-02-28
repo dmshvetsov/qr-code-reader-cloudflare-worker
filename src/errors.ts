d9c0a23dcddd8e33704625a4b9629ebd29377903
dmshvetsov <shvetsovdm@gmail.com>
2025-02-28 18:21:29 +0200

    fix typo

--- src/errors.ts
+++ src/errors.ts
@@ -10,7 +10,7 @@ export const CODE = {
 
 export const DESCRIPTION = {
   0: "something went wrong",
-  1001: "unable to download an file from given URL",
+  1001: "unable to download the file from given URL",
   1002: "unsupported image format or not an image",
   1003: `image size exceeds the limit of ${IMAGE_MAX_FILE_SIZE} bytes`,
   2001: "failed to read QR code, most likely given URL is not a QR code",
