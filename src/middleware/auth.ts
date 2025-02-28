70d4239e9ee01b92c8a66104aa6e2d843c11e310
dmshvetsov <shvetsovdm@gmail.com>
2025-02-28 20:43:38 +0200

    add protection from missing AUTH_TOKEN env var

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -3,6 +3,10 @@ import { bearerAuth as honoBearerAuth } from "hono/bearer-auth";
 export function bearerAuth() {
   return honoBearerAuth({
     async verifyToken(token, ctx) {
+      if (!ctx.env.AUTH_TOKEN) {
+        // no token, all requests unauthorized
+        return false;
+      }
       return ctx.env.AUTH_TOKEN === token;
     },
   });
